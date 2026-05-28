f289a1fb201f8dc392084fa8ee86fc3365ce6cd9
Shujaat Azim <shujaat.azim@gmail.com>
2026-05-28 18:31:06 -0400

    Add meta data to response, add param validation.

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,17 +1,40 @@
 class ReactionsController < ApplicationController
 
+  VALID_DIFFICULTIES = ["easy", "medium", "hard"]
+  VALID_CLASSIFICATIONS = ["reactant_missing", "solvent_missing", "product_missing"]
+
   def index
     reactions = Reaction.all
 
     if params[:difficulty].present?
+      unless VALID_DIFFICULTIES.include?(params[:difficulty])
+        return render json: {
+          error: "Invalid difficulty parameter, please use one of: easy, medium, or hard."
+        }, status: :bad_request
+      end
+
       reactions = reactions.where(difficulty: params[:difficulty])
     end
 
     if params[:classification].present?
+      unless VALID_CLASSIFICATIONS.include?(params[:classification])
+        return render json: {
+          error: "Invalid classification parameter, please use one of: reactant_missing, product_missing, or solvent_missing"
+        }, status: :bad_request
+      end
+
       reactions = reactions.where(classification: params[:classification])
     end
 
-    render json: reactions
+    render json: {
+      meta: {
+        count: reactions.count,
+        filters: {
+          difficulty: params[:difficulty],
+          classification: params[:classification]
+        }
+      },
+      data: reactions
+    }
   end
-  
 end
